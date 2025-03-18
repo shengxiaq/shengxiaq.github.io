f8eff657e4cac67457e2740c96c60c57b2c29b06
reg <50311735+shengxiaq@users.noreply.github.com>
2025-03-18 16:01:32 +0800

    Update exp.js

--- exp.js
+++ exp.js
@@ -10,7 +10,7 @@
 		xhr.open('POST', 'https://uis.mp.sohu.com/comment/api/comments/', true);
 		xhr.withCredentials = true;
 		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-		xhr.send('topic_title=%E2&topic_url=https%3A%2F%2Fwww.sohu.com%2F&reply_id=&topic_id=33045961&attachment_urls=&content=666&media_id=&source_id=' + source_id + '&channel_id=&requestId=');
+		xhr.send('topic_title=%E2&topic_url=https%3A%2F%2Fwww.sohu.com%2F&reply_id=&topic_id=33045961&attachment_urls=&content=666&media_id=&source_id=mp_' + source_id + '&channel_id=&requestId=');
 
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState == 4 && xhr.status == 200) {
